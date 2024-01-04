7e4c010de58b66b548578e0e32d62235d85f9933
viddheshborkar <viddhesh@gmail.com>
2024-01-04 09:35:08 +0530

    made changes to FindString

--- ArrayFindString.java
+++ ArrayFindString.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class ArrayFindString {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         String arr[] = {"Tiger", "Lion", "Fox", "Dog", "Cat","Deer","Horse"};
-        String value = "Deer";
+        System.out.println("Enter animal to be found");
+        String value = sc.next();
+
         boolean flag = false;
         for (int i=0; i<=arr.length-1;i++) {
             if (arr[i].equals(value)) {
